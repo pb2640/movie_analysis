eca73d64691998265d5fa67cff78f29ca38c2591
pb2640 <pb2640@nyu.edu>
2023-10-28 20:01:13 -0400

    Added runtime conversion function
    Converted runtime string to int

--- modules/analysis/functions.py
+++ modules/analysis/functions.py
@@ -20,39 +20,37 @@ from sklearn.preprocessing import MultiLabelBinarizer
 
 
 def make_new_columns_boxOffice(curr_df, col_name, list_of_keys):
-    """
-   
-    """
-    if(col_name not in curr_df.columns):
+    """ """
+    if col_name not in curr_df.columns:
         print("No such column exists in the df")
     for key in list_of_keys:
         curr_df[key] = ""
     for i in range(len(curr_df)):
         response_list = eval(curr_df["boxOffice"][i])
-        if(response_list):
+        if response_list:
             response = response_list
             for it in response:
                 for key in list_of_keys:
                     if key in it:
                         curr_df[key][i] = it[key]["val"]
-                        
 
     return curr_df
-    
-    # delete the original column 
+
+    # delete the original column
+
 
 def make_new_columns_first(curr_df, col_name, list_of_keys):
     """
     params will be(col_name,[list_of_keys]) ,
     the names in the list should be same as keys
     """
-    if (col_name not in curr_df.columns):
+    if col_name not in curr_df.columns:
         print("No such column exists in the df")
     for key in list_of_keys:
         curr_df[key] = ""
     for i in range(len(curr_df)):
         response_list = eval(curr_df["first"][i])
-        if (response_list):
+        if response_list:
             response = response_list
             for it in response:
                 for key in list_of_keys:
@@ -66,29 +64,28 @@ def make_new_columns_details(curr_df, col_name, list_of_keys):
     params will be(col_name,[list_of_keys]) ,
     the names in the list should be same as keys
     """
-    if(col_name not in curr_df.columns):
+    if col_name not in curr_df.columns:
         print("No such column exists in the df")
     for key in list_of_keys:
         curr_df[key] = ""
     for i in range(len(curr_df)):
         response_list = eval(curr_df["details"][i])
-        if(response_list):
+        if response_list:
             response = response_list
             for it in response:
                 for key in list_of_keys:
                     if key in it:
                         curr_df[key][i] = it[key]["val"]
-                        
 
     return curr_df
-   
+
 
 def money_str_to_currency(input_str):
-    '''Takes an input as a str. Saves currency signs if any\n",
-    saves value'''
-    if(len(input_str)==0):
+    """Takes an input as a str. Saves currency signs if any\n",
+    saves value"""
+    if len(input_str) == 0:
         return None
-    #strip the str of any spaces\n",
+    # strip the str of any spaces\n",
     stripped_input_str = input_str.strip()
     split_str = stripped_input_str.split()
     money_str = split_str[0]
@@ -97,38 +94,40 @@ def money_str_to_currency(input_str):
     # money = money_with_comma.replace(",","")
     return currency
 
+
 def money_str_to_int(input_str):
-    '''Takes an input as a str. Saves currency signs if any\n",
-    saves value'''
-    if(len(input_str)==0):
+    """Takes an input as a str. Saves currency signs if any\n",
+    saves value"""
+    if len(input_str) == 0:
         return None
-    #strip the str of any spaces\n",
+    # strip the str of any spaces\n",
     stripped_input_str = input_str.strip()
     split_str = stripped_input_str.split()
     money_str = split_str[0]
     currency = money_str[:1]
     money_with_comma = money_str[1:]
-    money = money_with_comma.replace(",","")
+    money = money_with_comma.replace(",", "")
     return money
+
 
 def extract_techspecs(input_str):
     try:
-        if (len(input_str) == 0):
+        if len(input_str) == 0:
             return [None, None, None]
         input_list = eval(input_str)
-        '''iterate on every object,
+        """iterate on every object,
         find every key in every object,
         if you find it place the value 
-        else place None'''
+        else place None"""
         runtime = None
         soundmix = None
         aspect_ratio = None
         for obj in input_list:
-            if ("title-techspec_runtime" in obj):
+            if "title-techspec_runtime" in obj:
                 runtime = obj["title-techspec_runtime"]["val"]
-            if ("title-techspec_soundmix" in obj):
+            if "title-techspec_soundmix" in obj:
                 soundmix = obj["title-techspec_soundmix"]["val"]
-            if ("title-techspec_aspectratio" in obj):
+            if "title-techspec_aspectratio" in obj:
                 aspect_ratio = obj["title-techspec_aspectratio"]["val"]
     except Exception as e:
         print(input_str, e)
@@ -142,13 +141,13 @@ def make_new_columns_details(orig_df, col_name, list_of_keys):
     the names in the list should be same as keys
     """
     curr_df = orig_df.copy()
-    if (col_name not in curr_df.columns):
+    if col_name not in curr_df.columns:
         print("No such column exists in the df")
     for key in list_of_keys:
         curr_df[key] = ""
     for i in range(len(curr_df)):
         response_list = eval(curr_df["details"][i])
-        if (response_list):
+        if response_list:
             response = response_list
             for it in response:
                 for key in list_of_keys:
@@ -161,10 +160,12 @@ def encode_variable(orig_df, col_name):
     mlb = MultiLabelBinarizer(sparse_output=True)
     curr_df = orig_df.copy()
     curr_df = curr_df.join(
-            pd.DataFrame.sparse.from_spmatrix(
-                mlb.fit_transform(curr_df.pop(col_name)),
-                index=curr_df.index,
-                columns=mlb.classes_))
+        pd.DataFrame.sparse.from_spmatrix(
+            mlb.fit_transform(curr_df.pop(col_name)),
+            index=curr_df.index,
+            columns=mlb.classes_,
+        )
+    )
     return curr_df
 
 
@@ -179,3 +180,48 @@ def extract_columns_from_dataframe(df, column_list):
             print("{} column not in the dataframe\n".format(column))
     df = df[column_confirmed_list]
     return df
+
+
+def convert_runtime_to_minutes_int(input_str):
+    """
+    takes input string eg. `1 hour 10 minutes`
+    initialize minutes_int = 0
+    Logic: split the string using keyword hour,
+    if the length of resulting list is 2:
+        Extract 1st item , strip it of white spaces
+        and convert it to int and multiply by 60 and
+        store in the minutes_int. make the list as
+        2nd item only
+    split the list on minutes and strip, convert
+    it to int and add it to minutes_ans
+
+    return minutes_int"""
+    if len(input_str) == 0:
+        return None
+    keyword_check_list = input_str.split(" ")
+    if "hour" in keyword_check_list:
+        hour_split_keyword = "hour"
+    else:
+        hour_split_keyword = "hours"
+    if "minutes" in keyword_check_list:
+        min_split_keyword = "minutes"
+    else:
+        min_split_keyword = "minute"
+    minutes_ans = 0
+    input_list = input_str.split(hour_split_keyword)
+    if len(input_list) == 2:
+        hour_str = input_list[0]
+        hour_str = hour_str.strip()
+        hour_int = int(hour_str)
+        minutes_ans += 60 * hour_int
+        input_str = input_list[1]
+        if input_str == "":
+            return minutes_ans
+    # extract minutes from str
+    input_list = input_str.split(min_split_keyword)
+    min_str = input_list[0]
+    min_str = min_str.strip()
+    min_int = int(min_str)
+    minutes_ans += min_int
+
+    return minutes_ans
--- modules/analysis/main.py
+++ modules/analysis/main.py
@@ -3,36 +3,79 @@ from functions import *
 import time
 
 
-
 if __name__ == "__main__":
     data_path = r"C:\Users\parth\2023\Fall 2023\Projects\docker_hack\data\data.csv"
     # data_path = '/home/server/2023/movies-project-oct/movies_project/data/csv_dumps/data.csv'
     df = pd.read_csv(data_path)
-    expanded_df = make_new_columns_boxOffice(df,"boxOffice", ["title-boxoffice-budget","title-boxoffice-cumulativeworldwidegross","title-boxoffice-grossdomestic","title-boxoffice-openingweekenddomestic"])
-    expanded_df = make_new_columns_first(expanded_df,'first',['title','poster_link','genres','plot','imdb-rating'])
-    expanded_df = make_new_columns_details(expanded_df,"details",['title-details-releasedate','title-details-origin','title-details-languages','title-details-filminglocations','title-details-companies'])
+    expanded_df = make_new_columns_boxOffice(
+        df,
+        "boxOffice",
+        [
+            "title-boxoffice-budget",
+            "title-boxoffice-cumulativeworldwidegross",
+            "title-boxoffice-grossdomestic",
+            "title-boxoffice-openingweekenddomestic",
+        ],
+    )
+    expanded_df = make_new_columns_first(
+        expanded_df, "first", ["title", "poster_link", "genres", "plot", "imdb-rating"]
+    )
+    expanded_df = make_new_columns_details(
+        expanded_df,
+        "details",
+        [
+            "title-details-releasedate",
+            "title-details-origin",
+            "title-details-languages",
+            "title-details-filminglocations",
+            "title-details-companies",
+        ],
+    )
     expanded_df["currency"] = None
     expanded_df["budget_money"] = None
-    expanded_df["currency"] = expanded_df["title-boxoffice-budget"].apply(money_str_to_currency)
-    expanded_df["budget_money"] = expanded_df["title-boxoffice-budget"].apply(money_str_to_int)
+    expanded_df["currency"] = expanded_df["title-boxoffice-budget"].apply(
+        money_str_to_currency
+    )
+    expanded_df["budget_money"] = expanded_df["title-boxoffice-budget"].apply(
+        money_str_to_int
+    )
     # create new column num_actors, fills in 0 where no value is present
-    expanded_df['num_actors'] = expanded_df['actors'].apply(lambda x: len(eval(x)))
-    expanded_df['num_genres'] = expanded_df['genres'].apply(lambda x: len(x))
-    expanded_df['imdb_rating'] = expanded_df['imdb-rating'].str.split('/').str[0]
-    expanded_df['imdb_rating'] = pd.to_numeric(expanded_df['imdb_rating'], errors='coerce').astype(float)
+    expanded_df["num_actors"] = expanded_df["actors"].apply(lambda x: len(eval(x)))
+    expanded_df["num_genres"] = expanded_df["genres"].apply(lambda x: len(x))
+    expanded_df["imdb_rating"] = expanded_df["imdb-rating"].str.split("/").str[0]
+    expanded_df["imdb_rating"] = pd.to_numeric(
+        expanded_df["imdb_rating"], errors="coerce"
+    ).astype(float)
     expanded_df["runtime"] = None
     expanded_df["soundmix"] = None
     expanded_df["aspect_ratio"] = None
     for i in range(len(expanded_df)):
         input_str = expanded_df["techspecs"][i]
-        runtime,soundmix,aspect_ratio = extract_techspecs(input_str)
+        runtime, soundmix, aspect_ratio = extract_techspecs(input_str)
         expanded_df["runtime"][i] = runtime
         expanded_df["soundmix"][i] = soundmix
         expanded_df["aspect_ratio"][i] = aspect_ratio
-    column_list = ['movie_id', 'title-boxoffice-budget', 'title', 'genres', 'plot', 'title-details-releasedate', 'title-details-origin', 
-                   'title-details-languages', 'title-details-filminglocations', 'title-details-companies', 'currency', 'budget_money', 
-                   'num_actors', 'num_genres', 'imdb_rating', 'runtime', 'soundmix', 'aspect_ratio']
+    column_list = [
+        "movie_id",
+        "title-boxoffice-budget",
+        "title",
+        "genres",
+        "plot",
+        "title-details-releasedate",
+        "title-details-origin",
+        "title-details-languages",
+        "title-details-filminglocations",
+        "title-details-companies",
+        "currency",
+        "budget_money",
+        "num_actors",
+        "num_genres",
+        "imdb_rating",
+        "runtime",
+        "soundmix",
+        "aspect_ratio",
+    ]
 
-    extract_df = extract_columns_from_dataframe(expanded_df,column_list)
+    extract_df = extract_columns_from_dataframe(expanded_df, column_list)
     timestr = time.strftime("%Y%m%d-%H%M%S")
     extract_df.to_csv("../../Data/csvs/{}.csv".format(timestr))
