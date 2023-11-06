e6fbe2d3f58dbc9f39f3bb149b29725161155983
pb2640 <pb2640@nyu.edu>
2023-11-06 13:40:48 -0500

    Added naive model inferencing

--- modules/model/functions.py
+++ modules/model/functions.py
@@ -16,6 +16,81 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+import sys
+import numpy as np
+from sklearn.preprocessing import MultiLabelBinarizer
+
+class ProcessDataframe():
+    def __init__(self, df_path) -> None:
+    
+        try:
+            self.df = pd.read_csv(df_path)
+        except Exception as e:
+            print("There was a problem in reading the file. The following error occured: {}".format(e))
+            sys.exit(1)
+
+    def remove_columns(self, columns_to_include: list):
+        columns_to_include_final = []
+        for col in columns_to_include:
+            if col in self.df.columns:
+                columns_to_include_final.append(col)
+
+        self.df = self.df[columns_to_include_final]
+
+    def remove_na_rows(self, na_col_list):
+        for col in na_col_list:
+            self.df = self.df[self.df[col].notna()]
+    def convert_str_to_list(self,input_str):
+        try:
+            return eval(input_str)
+        except:
+            return []
+    
+    def reduce_categorical_variables_dimensions(self, variable_name, threshold):
+        if(variable_name not in self.df.columns):
+            print("No such variable name")
+            return
+        if(isinstance(self.df[variable_name][self.df.index[0]],str)):
+            self.df[variable_name] = self.df[variable_name].apply(self.convert_str_to_list)
+        flattened_column = self.df[variable_name].explode()
+        try:    
+            column_value_counts = flattened_column.value_counts()
+        except Exception as e:
+            print("Make sure your column values are in a list format")
+            #take only those values which are greater than a threshold
+            return
+        try:
+            values_list = column_value_counts[column_value_counts>threshold].index
+        except:
+            print("Error with  the value counts obj")
+            return 
+        values_set = set(values_list)
+        # iterate over the df and 
+        # replace any value in the list if it is not in the values set
+        for i in self.df.index:
+            if(not self.df[variable_name][i]):
+                self.df[variable_name][i] = []
+                continue
+            curr_row = self.df[variable_name][i]
+            for j in range(len(curr_row)):
+                if(curr_row[j] not in values_set):
+                    curr_row[j] = "Others"
+            
+            self.df[variable_name][i] = list(set(self.df[variable_name][i]))
+    
+    def encode_variable(self, col_name):
+        mlb = MultiLabelBinarizer(sparse_output=True)
+        curr_df = self.df.copy()
+        new_df = curr_df.join(
+                pd.DataFrame.sparse.from_spmatrix(
+                    mlb.fit_transform(curr_df.pop(col_name)),
+                    index=curr_df.index,
+                    columns=mlb.classes_))
+        return new_df
+
+
+
+
 
 # Provide the dataset and the target variable
 class Model():
@@ -41,7 +116,7 @@ class Model():
         self.y = self.df[self.target_variable]  # Target variable
 
         try:
-            self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size, random_state=42)
+            self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=test_size, random_state=42)
         except Exception as e:
             print("The following error occured while preparing the dataset {}".format(e))
     
@@ -59,27 +134,32 @@ class Model():
 
     def test_model(self):
         self.y_pred = self.model.predict(self.X_test)
-        self.mse = mean_squared_error(self.y_test, self.y_pred)
-        self.r2 = r2_score(self.y_test, self.y_pred)
+        self.test_mse = mean_squared_error(self.y_test, self.y_pred)
+        self.test_r2 = r2_score(self.y_test, self.y_pred)
         
 
     def report_model_results(self):
-        self.model_results = {"mse": self.mse,"r2_score":self.r2}
-        print(pd.DataFrame)
+        print("Time taken to train the model {} seconds".format(self.total_time_taken_to_train_model))
+        self.model_results = {"test_mse": self.test_mse, "test_r2_score": self.test_r2 }
+        print(self.model_results)
 
 
-    def inference(self, input_list):
+    def inference(self, input_dict : dict):
         '''
         Provide the input to the model and get the prediction
         '''
-        self.inference_input = input_list
+        self.inference_input_dict = input_dict
+        inference_input_list = [0]*len(self.X.columns)
+        for it in input_dict:
+            if it in self.X.columns:
+                index = self.X.columns.get_loc(it)
+                inference_input_list[index] = input_dict[it]
         try:
-            self.inference_prediction = self.model.predict(input_list)
+            inference_input_list = np.array(inference_input_list)
+            self.inference_prediction = self.model.predict(inference_input_list.reshape(1, -1))
         except Exception as e:
-            print("The following error occured while inferencing the model {}".format(e))
-            return None
-        
-        
+            print("The following error occured while inferencing the model : {}".format(e))
+            return None     
         return self.inference_prediction
 
 
---
+++ modules/model/main.py
@@ -0,0 +1,24 @@
+import pandas as pd
+from functions import Model, ProcessDataframe
+
+
+if __name__ == "__main__":
+    #process dataframe
+    data_path = r"C:\Users\parth\2023\Fall 2023\Projects\movie_analysis\data\csvs\data2.csv"
+    df = ProcessDataframe(data_path)
+    df.remove_columns(["release_year","runtime_int","num_actors","budget_money","title-details-companies"])
+    df.remove_na_rows(["budget_money","runtime_int"])
+    df.reduce_categorical_variables_dimensions("title-details-companies",10)
+    prepared_df = df.encode_variable("title-details-companies")
+
+    print(prepared_df.head())
+    model = Model(prepared_df, "budget_money", "regression")
+    model.prepare_train_test_datasets()
+    model.train_model()
+    model.test_model()
+    model.report_model_results()
+
+
+
+
+    print("Done")
