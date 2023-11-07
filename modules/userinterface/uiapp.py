5b040f350a130add1f5b62e807fcf35545556f77
AMAN-9608 <amandeep9608@gmail.com>
2023-11-06 21:09:34 -0500

    updated uiapp.py

--- modules/userinterface/uiapp.py
+++ modules/userinterface/uiapp.py
@@ -27,25 +27,83 @@ def get_data():
 
 
 with open(model_file, 'rb') as file:
-    loaded_model = pickle.load(file)
+    dv, loaded_model = pickle.load(file)
 
-col1, col2, col3, col4 = st.columns(4)
+
+col1, col2, col3, col4, col5, col6= st.columns(6)
 
 with col1:
-    genre = st.multiselect('Select the Genre(s) you\'d like to have',['Action','Adventure','Anime'])
+    genres_list =  ['Action', 'Adult', 'Adventure', 'Animation', 'Biography', 'Comedy',
+                    'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'Film-Noir',
+                    'Game-Show', 'History', 'Horror', 'Music', 'Musical', 'Mystery',
+                    'News', 'Reality-TV', 'Romance', 'Sci-Fi', 'Short', 'Sport',
+                    'Talk-Show', 'Thriller', 'War', 'Western']
+    genre = st.multiselect('Select the Genre(s) you\'d like to have', genres_list)
 
 with col2:
-    num_actors = st.number_input('Enter the number of actors you\'d like to have',step=1)
+    num_actors = st.number_input('Enter the number of actors',step=1, value=10)
 
 with col3:
-    runtime = st.number_input('Enter the runtime (in minutes) of the movie',step=10)
+    runtime = st.number_input('Enter the movie runtime (in minutes)',step=10, value=90)
 
 with col4:
-    langs = st.multiselect('Select the Language(s) you\'d like to have',['English','French','Mandarin'])
+    common_langs = ['English','French','Spanish','German','Arabic','Hindi','Italian','Chinese','Mandarin','Russian', 'Latin', 'Japanese', 'Persian', 'Bengali']
+    langs = st.multiselect('Select the Language(s)',common_langs)
+    
+
+with col5:
+    release_year = st.number_input('Select the release year of the movie', min_value=2022, max_value=2030)
+    release_year = int(release_year)
+    
+
+with col6:
+    company_list = ['Amblin Entertainment', 'American Film Institute (AFI)',
+                    'Bigfott Studios', 'Blumhouse Productions', 'BondIt Media Capital',
+                    'Columbia Pictures', 'Cyfuno Ventures', 'El Dorado Films',
+                    'Entertain Me Productions', 'JC Films', 'Mattioli Productions',
+                    'Metro-Goldwyn-Mayer (MGM)', 'North Bank Entertainment', 'Others',
+                    'Paramount Pictures', 'Reality Entertainment (RE)',
+                    'Small Town Monsters.', 'Twentieth Century Fox', 'Universal Pictures',
+                    'University of Southern California, School of Cinematic Arts',
+                    'Veteran Documentary Corps', 'Walt Disney Pictures', 'Warner Bros.',
+                    'Weston Woods Studios']
+
+    prod_company = st.multiselect('Select the production company', company_list)
 
 
+user_input = dict()
+user_input['runtime'] = runtime
+user_input['num_actors'] = num_actors
+user_input['release_year'] = release_year
+for i in genre:
+    user_input[i] = 1
+for i in langs:
+    user_input[i] = 1
+for i in prod_company:
+    user_input[i] = 1
+
+
+input_df = pd.DataFrame(columns=['num_actors', 'runtime', 'Action', 'Adult', 'Adventure', 'Animation', 'Biography', 'Comedy',
+        'Crime', 'Documentary', 'Drama', 'Family', 'Fantasy', 'Film-Noir', 'Game-Show', 'History', 'Horror', 'Music', 'Musical',
+        'Mystery', 'News', 'Reality-TV', 'Romance', 'Sci-Fi', 'Short', 'Sport', 'Talk-Show', 'Thriller', 'War', 'Western', 'Arabic',
+        'Bengali', 'Chinese', 'English', 'French', 'German', 'Hindi', 'Italian', 'Japanese', 'Mandarin', 'Persian', 'Russian', 'Spanish',
+        'Amblin Entertainment', 'American Film Institute (AFI)', 'Bigfott Studios', 'Blumhouse Productions', 'BondIt Media Capital', 'Columbia Pictures',
+        'Cyfuno Ventures', 'El Dorado Films', 'Entertain Me Productions', 'JC Films', 'Mattioli Productions', 'Metro-Goldwyn-Mayer (MGM)', 'North Bank Entertainment',
+        'Others', 'Paramount Pictures', 'Reality Entertainment (RE)', 'Small Town Monsters.', 'Twentieth Century Fox', 'Universal Pictures', 'University of Southern California, School of Cinematic Arts',
+        'Veteran Documentary Corps', 'Walt Disney Pictures', 'Warner Bros.', 'Weston Woods Studios', 'release_year'])
+
+input_df.loc[len(input_df)] = user_input
+input_df = input_df.fillna(0)
+# input_df['num_actors'] = input_df['num_actors'].astype('int')
+# input_df['runtime'] = input_df['runtime'].astype('int')
+# input_df['release_year'] = input_df['release_year'].astype('int')
+
+
+if st.button('Predict'):
+    if not (genre and langs and prod_company):
+        st.error('Kindly make sure all selections have been made!', icon="🚨")
+    else:
+        val = round(loaded_model.predict(input_df)[0])
+        st.markdown(f'The estimated budget of your movie is **${val:,}**.')
 
     
-# create sample with data
-# encode genres and languages
-# pass to model for prediction
